bf9ce3731c02b4ce5386d8054e5832b7e80d2baf
panduputrapratama@smkwikrama.sch.id <panduputrapratama@smkwikrama.sch.id>
2024-06-13 18:36:34 +0700

    laravel

---
+++ resources/views/Porto.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pandu Putra Pratama</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="#" class="logo">Pandu.P</a>
+
+        <nav class="navbarr">
+            <a href="#">Home</a>
+            <a href="#1">About</a>
+            <a href="#2">My Skills</a>
+            <a href="#3">Contact</a>
+        </nav>
+
+    </header>
+
+    <section class="home">
+        <div class="home-image">
+            <img src="img/a.jpeg" alt="">
+        </div>
+
+
+        <div class="home-content">
+            <h1>Hi,I'm Pandu Putra Pratama</h1>
+            <h3>30-03-2008</h3>
+            <p>Frontend Developer</p>
+
+            <div class="home-sci">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+                <a href="#"><i class='bx bxl-whatsapp'></i></a>
+            </div>
+
+            <a href="#" class="btn">Download CV</a>
+        </div>
+    </section>
+
+    <section class="section 2" id="1">
+        <div class="About">
+            <img src="img/p.jpeg" alt="">
+
+            <div class="text">
+                <h1>About Me</h1>
+                <p>Saya Pandu Putra Pratama,Saya berumur 15 Tahun, Saya Tinggal Di Cibedug
+                    Saya Sekolah Di Smk Wikrama Bogor,Hobi Saya Bermain Sepak BOla
+                </p>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="section 3" id="2">
+        <div class="boxer">
+            <div class="box1">
+                <a href="#"><i class='bx bxl-html5'></i></a>
+                <div>
+                    <p> HTML adalah bahasa markah yang digunakan peramban untuk menafsirkan dan menulis teks, gambar dan
+                        bahan lainnya ke dalam halaman web secara visual maupun suara. Karakteristik dasar untuk setiap
+                        item dari markah HTML didefinisikan di dalam peramban, dan karakteristik ini dapat diubah atau
+                        ditingkatkan dengan menggunakan tambahan halaman web desainer CSS.</p>
+                    <button>
+                        <a href="https://id.wikipedia.org/wiki/HTML">Telusuri</a>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <h1>My Skills</h1>
+            </div>
+            <div class="box2">
+                <a href="#"><i class='bx bxl-css3'></i></a>
+                <div>
+                    <p>HTML dan CSS memiliki keterikatan yang erat. Karena HTML adalah bahasa markup (fondasi situs) dan
+                        CSS memperbaiki style (untuk semua aspek yang terkait dengan tampilan website), maka kedua
+                        bahasa pemrograman ini harus berjalan beriringan.</p>
+                    <button>
+                        <a href="https://en.wikipedia.org/wiki/CSS">Telusuri</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</body>
+
+</html>
